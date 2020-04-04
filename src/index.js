15821dd1d1fe725871b9aa376909e1d3c8eb4809
My Name <myEmail@example.com>
2020-04-04 15:54:40 +0300

    Version2

--- src/index.js
+++ src/index.js
@@ -2,75 +2,77 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-class Square extends React.Component { // Клетка
-    constructor(props){ // 1 добавим конструктор к классу, чтобы инициализировать состояние
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
+}
+  
+class Board extends React.Component {
+    constructor(props) {
         super(props)
         this.state = {
-            valve: null
+            squares: Array(9).fill(null)
         }
     }
-    render() {
-      return (
-        <button 
-            className="square" 
-            onClick={() => this.setState({ value: 'X'})} // 2 мы говорим , что нужно перерендерить Square при каждом клике по <button>
-        >
-            {this.state.value}
-        </button>
-      );
-    }
-  }
   
-  class Board extends React.Component { // Поле
+    handleClick(i) {
+        const squares = this.state.squares.slice()
+        squares[i] = 'X'
+        this.setState({ squares: squares })
+    }
+  
     renderSquare(i) {
-      return <Square value={i} />;
+        return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />
     }
   
     render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
+        const status = 'Next player: X'
+    
+        return (
+            <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {this.renderSquare(6)}
+                {this.renderSquare(7)}
+                {this.renderSquare(8)}
+            </div>
+            </div>
+        )
     }
-  }
+}
   
-  class Game extends React.Component { // Игра
+class Game extends React.Component { // Игра
     render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
+        return (
+            <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+            <div className="game-info">
+                <div>{/* status */}</div>
+                <ol>{/* TODO */}</ol>
+            </div>
+            </div>
+        );
     }
-  }
+}
   
   // ========================================
   
-  ReactDOM.render(
+ReactDOM.render(
     <Game />,
     document.getElementById('root')
-  );
+);
